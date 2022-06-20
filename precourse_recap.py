5a4236ffacf3f8bfea82d0fec2c34939b98c10fc
CarlyCoding <carly_off@outlook.com>
2022-06-20 20:06:03 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,13 @@
+tax_details = "Everyone is entitled, prior to tax calculation of an allowance of "
+personal_allowance = "£12,570 which is tax free earning."
+salary_1 = 34000
+p_allow_fig = 12570
+s_vat = 0.2
+
+print(tax_details + personal_allowance )
+print("For example a salary of £34,00 would have a taxable figure of:")
+print(salary_1 - p_allow_fig)
+
+
+
+
